d249bbd7aa8fe0f85e72ae7cdf778b617b7034ec
Owlbear <AnOwlbear@protonmail.com>
2020-02-08 16:28:52 +0000

    Improved conversion of json to object, improved tests.

--- PixivCSharp.Tests/Login.cs
+++ PixivCSharp.Tests/Login.cs
@@ -8,7 +8,12 @@ namespace PixivCSharp.Tests
         static void Main(string[] args)
         {
             client = new Pixiv();
-            Walkthrough();
+            IllustSearchResult walkthough = client.WalkthoughIllusts();
+
+            foreach (Illust illust in walkthough.illusts)
+            {
+                Console.WriteLine(illust.sanity_level);
+            }
         }
 
         static void Walkthrough()
@@ -22,37 +27,36 @@ namespace PixivCSharp.Tests
                 Console.WriteLine("-------------------------------------------------------------------------------");
                 Console.WriteLine("Illust:");
                 Console.WriteLine("-------------------------------------------------------------------------------");
-                Console.WriteLine(illust.id.ToString());
-                Console.WriteLine(illust.title);
-                Console.WriteLine(illust.type);
-                Console.WriteLine(illust.image_urls.large);
-                Console.WriteLine(illust.caption);
-                Console.WriteLine(illust.restrict.ToString());
+                Console.WriteLine("Illust ID: {0}", illust.id.ToString());
+                Console.WriteLine("Illust title: {0}", illust.title);
+                Console.WriteLine("Illust type: {0}", illust.type);
+                Console.WriteLine("Illust image urls: {0}", illust.image_urls.large);
+                Console.WriteLine("Illust caption: {0}", illust.caption);
+                Console.WriteLine("Illust restrict: {0}", illust.restrict.ToString());
                 Console.WriteLine("-------------------------------------------------------------------------------");
                 Console.WriteLine("User:");
                 Console.WriteLine("-------------------------------------------------------------------------------");
-                Console.WriteLine(illust.user.id.ToString());
-                Console.WriteLine(illust.user.name);
-                Console.WriteLine(illust.user.account);
-                Console.WriteLine(illust.user.profile_image_urls.medium);
-                Console.WriteLine(illust.user.is_followed);
+                Console.WriteLine("User id: {0}", illust.user.id.ToString());
+                Console.WriteLine("User name: {0}", illust.user.name);
+                Console.WriteLine("User account {0}", illust.user.account);
+                Console.WriteLine("User profile picture url: {0}", illust.user.profile_image_urls.medium);
+                Console.WriteLine("Is user followed: {0}", illust.user.is_followed);
                 Console.WriteLine("-------------------------------------------------------------------------------");
-                Console.WriteLine("{0} | {1}", illust.tags[0].name, illust.tags[0].translated_name);
-                if (illust.tools.Length != 0) Console.WriteLine(illust.tools[0]);
-                Console.WriteLine(illust.create_date);
-                Console.WriteLine(illust.page_count.ToString());
-                Console.WriteLine(illust.page_count.ToString());
-                Console.WriteLine(illust.width.ToString());
-                Console.WriteLine(illust.height.ToString());
-                Console.WriteLine(illust.sanity_level.ToString());
-                Console.WriteLine(illust.x_restrict.ToString());
+                Console.WriteLine("Illust tag: {0} | Illust translated tag: {1}", illust.tags[0].name, illust.tags[0].translated_name);
+                if (illust.tools.Length != 0) Console.WriteLine("Illust tools: {0}", illust.tools[0]);
+                Console.WriteLine("Illust creation date: {0}", illust.create_date);
+                Console.WriteLine("Illust page count: {0}", illust.page_count.ToString());
+                Console.WriteLine("Illust width: {0}", illust.width.ToString());
+                Console.WriteLine("Illust height: {0}", illust.height.ToString());
+                Console.WriteLine("Illust sanity level: {0}", illust.sanity_level.ToString());
+                Console.WriteLine("Illust x restrict: {0}", illust.x_restrict.ToString());
                 Console.WriteLine("-------------------------------------------------------------------------------");
                 Console.WriteLine("Series:");
                 Console.WriteLine("-------------------------------------------------------------------------------");
                 if (illust.series != null)
                 {
-                    Console.WriteLine(illust.series.id);
-                    Console.WriteLine(illust.series.title);
+                    Console.WriteLine("Series ID: {0}", illust.series.id);
+                    Console.WriteLine("Series title: {0}", illust.series.title);
                 }
                 else
                 {
@@ -61,20 +65,20 @@ namespace PixivCSharp.Tests
                 Console.WriteLine("-------------------------------------------------------------------------------");
                 if (illust.meta_single_page != null)
                 {
-                    Console.WriteLine(illust.meta_single_page.original_image_url);
+                    Console.WriteLine("Page url: {0}", illust.meta_single_page.original_image_url);
                 }
                 else if (illust.meta_single_page == null)
                 {
-                    Console.WriteLine(illust.meta_pages[0].image_urls.original);
+                    Console.WriteLine("First page url: {0}", illust.meta_pages[0].image_urls.original);
                 }
-                Console.WriteLine(illust.total_view.ToString());
-                Console.WriteLine(illust.total_bookmarks.ToString());
-                Console.WriteLine(illust.visible);
-                Console.WriteLine(illust.is_muted);
+                Console.WriteLine("Illust view count: {0}", illust.total_view.ToString());
+                Console.WriteLine("Illust bookmarks: {0}", illust.total_bookmarks.ToString());
+                Console.WriteLine("Is illust visible: {0}", illust.visible);
+                Console.WriteLine("Is illust muted: {0}", illust.is_muted);
                 Console.WriteLine("-------------------------------------------------------------------------------");
             }
 
-            Console.WriteLine(walkthough.next_url);
+            Console.WriteLine("Next url: {0}", walkthough.next_url);
             Console.WriteLine("\n\n\n");
         }
     }
--- PixivCSharp/PixivCSharp.cs
+++ PixivCSharp/PixivCSharp.cs
@@ -19,41 +19,7 @@ namespace PixivCSharp
             //converts json to IllustSearchResults object and returns object
             foreach (JToken illust in illusts)
             {
-                Illust newIllust = new Illust
-                {
-                    id = (int)illust["id"],
-                    title = (string)illust["title"],
-                    type = (string)illust["type"],
-                    image_urls = (illust["image_urls"]).ToObject<IllustImageUrls>(),
-                    caption = (string)illust["caption"],
-                    restrict = (int)illust["restrict"],
-                    user =
-                        new User()
-                        {
-                            id = (int)illust["user"]["id"],
-                            name = (string)illust["user"]["name"],
-                            account = (string)illust["user"]["account"],
-                            profile_image_urls = (illust["user"]["profile_image_urls"]).ToObject<ImageUrls>(),
-                            is_followed = (bool)illust["user"]["is_followed"]
-                        },
-                    tags = ((JArray)illust["tags"]).ToObject<Tag[]>(),
-                    tools = ((JArray)illust["tools"]).ToObject<string[]>(),
-                    create_date = (string)illust["create_date"],
-                    page_count = (int)illust["page_count"],
-                    width = (int)illust["width"],
-                    height = (int)illust["height"],
-                    sanity_level = (int)illust["sanity_level"],
-                    x_restrict = (int)illust["x_restrict"],
-                    series = (illust["series"]).ToObject<Series>(),
-                    meta_single_page = (illust["meta_single_page"]).ToObject<MetaSinglePage>(),
-                    meta_pages = (illust["meta_pages"]).ToObject<MetaPages[]>(),
-                    total_view = (int)illust["total_view"],
-                    total_bookmarks = (int)illust["total_bookmarks"],
-                    is_bookmarked = (bool)illust["is_bookmarked"],
-                    visible = (bool)illust["visible"],
-                    is_muted = (bool)illust["is_muted"]
-                };
-
+                Illust newIllust = (illust.ToObject<Illust>());
                 result.illusts.Add(newIllust);
             }
             result.next_url = (string)json["next_url"];
