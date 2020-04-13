9f1a0dc7d42b7323d4ffef275095ae72bc9f3323
Owlbear <AnOwlbear@protonmail.com>
2020-04-13 12:21:34 +0100

    Added methods to deserialize stream to json.

--- PixivCSharp/Json.cs
+++ PixivCSharp/Json.cs
@@ -1,14 +1,34 @@
+using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace PixivCSharp
 {
-    static class Json
+    internal static class Json
     {
         public static T DeserializeJson<T>(string json)
         {
             T result = JsonConvert.DeserializeObject<T>(json);
             return result;
+        }
+
+        public static T DeserializeJson<T>(Stream json)
+        {
+            using (StreamReader sr = new StreamReader(json))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                return serializer.Deserialize<T>(reader);
+            }
+        }
+
+        public static T DeserializeJson<T>(Stream json, string target)
+        {
+            using (StreamReader reader = new StreamReader(json))
+            {
+                JObject jObject = JObject.Parse(reader.ReadToEnd());
+                return jObject[target].ToObject<T>();
+            }
         }
 
         public static T DeserializeJson<T>(JObject json)
