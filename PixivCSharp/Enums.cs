7af6175045c2067a53ac0d5dc7e665c91c5a6edf
An-Owlbear <AnOwlbear@protonmail.com>
2020-05-08 11:15:42 +0100

    Created JsonValueAttribute and added exntension method and attributes to enum.

--- PixivCSharp/Enums.cs
+++ PixivCSharp/Enums.cs
@@ -3,41 +3,61 @@
     public enum Filter
     {
         None,
+        [JsonValue("for_android")]
         Android,
+        [JsonValue("for_ios")]
         IOS
     }
 
     public enum Publicity
     {
+        [JsonValue("public")]
         Public,
+        [JsonValue("private")]
         Private,
+        [JsonValue("mypixiv")]
         MyPixiv
     }
 
     public enum Sort
     {
+        [JsonValue("date_desc")]
         DateDesc,
+        [JsonValue("date_asc")]
         DateAsc
     }
 
     public enum SearchTarget
     {
+        [JsonValue("partial_match_for_tags")]
         PartialTagMatch,
+        [JsonValue("exact_match_for_tags")]
         ExactTagMatch,
+        [JsonValue("title_and_caption")]
         TitleAndCaption
     }
 
     public enum RankingMode
     {
+        [JsonValue("day")]
         Day,
+        [JsonValue("week")]
         Week,
+        [JsonValue("month")]
         Month,
+        [JsonValue("day_male")]
         DayMale,
+        [JsonValue("day_female")]
         DayFemale,
+        [JsonValue("week_original")]
         WeekOriginal,
+        [JsonValue("day_manga")]
         DayManga,
+        [JsonValue("week_rookie_manga")]
         WeekRookieManga,
+        [JsonValue("week_manga")]
         WeekManga,
+        [JsonValue("month_manga")]
         MonthManga
     }
 }
---
+++ PixivCSharp/JsonValueAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace PixivCSharp
+{
+    /// <summary>
+    /// Attribute used to represent the Json value of an enum.
+    /// </summary>
+    public class JsonValueAttribute : Attribute
+    {
+        /// <summary>
+        /// Holds the json value of an enum.
+        /// </summary>
+        public string JsonValue { get; protected set; }
+
+        /// <summary>
+        /// Constructor for JsonValueAttribute.
+        /// </summary>
+        /// <param name="value">The json value.</param>
+        public JsonValueAttribute(string value)
+        {
+            JsonValue = value;
+        }
+    }
+
+    /// <summary>
+    /// Contains extension methods.
+    /// </summary>
+    public static class ExtensionMethods
+    {
+        public static string JsonValue(this Enum value)
+        {
+            Type type = value.GetType();
+            FieldInfo fieldInfo = type.GetField(value.ToString());
+
+            JsonValueAttribute[] attribs = fieldInfo.GetCustomAttributes(typeof(JsonValueAttribute), false) as JsonValueAttribute[];
+
+            return attribs.Length > 0 ? attribs[0].JsonValue : null;
+        }
+    }
+}
