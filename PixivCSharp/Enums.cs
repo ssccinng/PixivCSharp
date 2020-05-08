0d38067d39f2986460183e3a62c9ecbc7a8c767b
An-Owlbear <AnOwlbear@protonmail.com>
2020-05-08 10:46:25 +0100

    Added enums.

---
+++ PixivCSharp/Enums.cs
@@ -0,0 +1,43 @@
+﻿namespace PixivCSharp
+{
+    public enum Filter
+    {
+        None,
+        Android,
+        IOS
+    }
+
+    public enum Publicity
+    {
+        Public,
+        Private,
+        MyPixiv
+    }
+
+    public enum Sort
+    {
+        DateDesc,
+        DateAsc
+    }
+
+    public enum SearchTarget
+    {
+        PartialTagMatch,
+        ExactTagMatch,
+        TitleAndCaption
+    }
+
+    public enum RankingMode
+    {
+        Day,
+        Week,
+        Month,
+        DayMale,
+        DayFemale,
+        WeekOriginal,
+        DayManga,
+        WeekRookieManga,
+        WeekManga,
+        MonthManga
+    }
+}
